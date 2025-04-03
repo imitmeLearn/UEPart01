491fce224badceadffac355670d22674b4a4f2a4
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-03 16:36:22 +0900

    (완) TArray / TSet

--- UnrealContainer/Source/UnrealContainer/MyGameInstance.cpp
+++ UnrealContainer/Source/UnrealContainer/MyGameInstance.cpp
@@ -45,4 +45,13 @@ void UMyGameInstance::Init()
 	UE_LOG(LogTemp, Log, TEXT("=====합계========="));
 	int32 SumByAlgo = Algo::Accumulate(IntArray, 0);
 	UE_LOG(LogTemp, Log, TEXT("SumByAlgo: %d "), SumByAlgo);	//출력
+
+	UE_LOG(LogTemp, Log, TEXT("=====TSet======="));
+	TSet<int32> IntSet;	//TArray
+	for (int ix = 0; ix < 10; ix++)
+	{
+		IntSet.Add(ix + 1);
+	}	//동적 배열에 자료 추가.
+
+	IntSet.Remove(2);
 }
