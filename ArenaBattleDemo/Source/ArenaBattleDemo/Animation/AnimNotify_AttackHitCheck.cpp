ece56b9c751c0ee8f1a5c13969cca0af2f7dabfb
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-17 10:07:17 +0900

    0417-01-노티파이 선언

---
+++ ArenaBattleDemo/Source/ArenaBattleDemo/Animation/AnimNotify_AttackHitCheck.cpp
@@ -0,0 +1,8 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Animation/AnimNotify_AttackHitCheck.h"
+
+void UAnimNotify_AttackHitCheck::Notify(USkeletalMeshComponent* MeshComp,UAnimSequenceBase* Animation,const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp,Animation,EventReference);
+}
