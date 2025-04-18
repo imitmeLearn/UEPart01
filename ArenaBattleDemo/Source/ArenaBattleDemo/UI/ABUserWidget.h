e916e1d5b75f32e88e18b071f505b80d9ada0f2b
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-18 14:31:43 +0900

    0418-07-캐릭터 스탯과 위젯 - 01 유저위젯 - 확장 생성

---
+++ ArenaBattleDemo/Source/ArenaBattleDemo/UI/ABUserWidget.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ABUserWidget.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class ARENABATTLEDEMO_API UABUserWidget: public UUserWidget
+{
+	GENERATED_BODY()
+public:
+
+protected:
+};
---
+++ ArenaBattleDemo/Source/ArenaBattleDemo/UI/ABWidgetComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "ABWidgetComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLEDEMO_API UABWidgetComponent : public UWidgetComponent
+{
+	GENERATED_BODY()
+	
+};
