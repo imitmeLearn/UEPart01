4cfe50cfca7924017d1c6ae2f717350697847c3c
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-21 10:15:04 +0900

    0421 - 08 - 아이템시스템 - 트리거박스구현 - 생성

---
+++ ArenaBattleDemo/Source/ArenaBattleDemo/Item/ABItemBox.cpp
@@ -0,0 +1,8 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Item/ABItemBox.h"
+
+// Sets default values
+AABItemBox::AABItemBox()
+{
+}
---
+++ ArenaBattleDemo/Source/ArenaBattleDemo/Item/ABItemBox.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ABItemBox.generated.h"
+
+UCLASS()
+class ARENABATTLEDEMO_API AABItemBox: public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AABItemBox();
+
+protected:
+
+public:
+};
