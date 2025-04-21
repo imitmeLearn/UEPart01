6c4312e47fb2561cf519e4aad5b376ef3223739b
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-21 11:25:10 +0900

    0421 - 08 - 아이템시스템 - 트리거박스구현 -데이터 생성

---
+++ ArenaBattleDemo/Source/ArenaBattleDemo/Item/ABItemData.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ABItemData.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class ARENABATTLEDEMO_API UABItemData: public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+};
