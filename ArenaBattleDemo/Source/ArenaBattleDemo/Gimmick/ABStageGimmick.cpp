7a99360ac325e9ddcb8926d2946596debda17716
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-22 11:20:35 +0900

    0422 - 09 - 스테이지기믹 - 스테이지STAGE 생 , GATE -스테이지 소켓에 문 붙이기 , BP생성 - AABStageGimmick class 로!

--- ArenaBattleDemo/Source/ArenaBattleDemo/Gimmick/ABStageGimmick.cpp
+++ ArenaBattleDemo/Source/ArenaBattleDemo/Gimmick/ABStageGimmick.cpp
@@ -1,8 +1,85 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Gimmick/ABStageGimmick.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+#include "Physics/ABCollision.h"
 
 // Sets default values
 AABStageGimmick::AABStageGimmick()
 {
+	////STAGE SECTION
+	Stage = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Stage"));
+	RootComponent = Stage;
+
+	//리소스 설정
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> StageMeshRef(TEXT("/Game/ArenaBattle/Environment/Stages/SM_SQUARE.SM_SQUARE"));
+	if(StageMeshRef.Object)
+	{
+		Stage ->SetStaticMesh(StageMeshRef.Object);
+	}
+
+	StageTrigger =  CreateDefaultSubobject<UBoxComponent>(TEXT("StageTrigger"));
+	StageTrigger -> SetBoxExtent(FVector(775.f,775.f,300.f));
+	StageTrigger -> SetupAttachment(Stage);
+	StageTrigger -> SetRelativeLocation(FVector(0.f,0.f,250.f));
+	StageTrigger -> SetCollisionProfileName(CPROFILE_ABTRIGGER);
+	StageTrigger -> OnComponentBeginOverlap.AddDynamic(this,&AABStageGimmick::OnStageTriggerBeginOverlap);
+
+	////GATE SETCION
+	static FName GateSockets[] = {
+		TEXT("+XGate")
+		,TEXT("-XGate")
+		,TEXT("+YGate")
+		,TEXT("-YGate")
+	};
+
+	//리소스 로드
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> GateMeshRef(TEXT("/Game/ArenaBattle/Environment/Props/SM_GATE.SM_GATE"));
+
+	//게이트 생성
+	for(const FName& GateSocket : GateSockets)
+	{
+		//컴포넌트 생성
+		UStaticMeshComponent* Gate = CreateDefaultSubobject<UStaticMeshComponent>(GateSocket);
+
+		//셍성한 스테틱 매시 컴포넌트 에셋 설정
+		if(GateMeshRef.Object)
+		{
+			Gate->SetStaticMesh(GateMeshRef.Object);
+		}
+
+		//컴포넌트 설정.
+		Gate->SetupAttachment(Stage,GateSocket);			//부모 설정
+		Gate->SetRelativeLocation(FVector(0.f,-80.f,0.f));	//위치 설정
+		Gate->SetRelativeRotation(FRotator(0.f,-90.f,0.f));	//회전 설정
+
+		//생성한 컴포넌트 맵에 추가
+		Gates.Add(GateSocket,Gate);
+
+		//문마다 배치할 BoxComponent 생성 및 설정.
+		FName TriggerName = *GateSocket.ToString().Append("Trigger");
+
+		//컴포넌트 생성.
+		UBoxComponent* GateTrigger = CreateDefaultSubobject<UBoxComponent>(TriggerName);
+
+		// 컴포넌트 설정.
+		GateTrigger->SetBoxExtent(FVector(100.0f,100.0f,300.0f));
+		GateTrigger->SetupAttachment(Stage,GateSocket);
+		GateTrigger->SetRelativeLocation(FVector(70.0f,0.0f,250.0f));
+		GateTrigger->SetCollisionProfileName(CPROFILE_ABTRIGGER);
+		GateTrigger->OnComponentBeginOverlap.AddDynamic(this,&AABStageGimmick::OnGateTriggerBeginOverlap);
+
+		// 태그 설정.
+		GateTrigger->ComponentTags.Add(GateSocket);
+
+		//생성한 컴포넌트 배열에 추가.
+		GateTriggers.Add(GateTrigger);
+	}
 }
+
+void AABStageGimmick::OnStageTriggerBeginOverlap(UPrimitiveComponent * OverlappedComponent,AActor * OtherActor,UPrimitiveComponent * OtherComp,int32 OtherBodyIndex,bool bFromSweep,const FHitResult & SweepResult)
+{}
+
+void AABStageGimmick::OnGateTriggerBeginOverlap(UPrimitiveComponent * OverlappedComponent,AActor * OtherActor,UPrimitiveComponent * OtherComp,int32 OtherBodyIndex,bool bFromSweep,const FHitResult & SweepResult)
+{}
--- ArenaBattleDemo/Source/ArenaBattleDemo/Gimmick/ABStageGimmick.h
+++ ArenaBattleDemo/Source/ArenaBattleDemo/Gimmick/ABStageGimmick.h
@@ -15,7 +15,42 @@ public:
 	// Sets default values for this actor's properties
 	AABStageGimmick();
 
-protected:
+protected:	//STAGE SECTION
 
+	// 스테이지를 보여줄 때 사용할 메시 컴포넌트.
+	UPROPERTY(VisibleAnywhere,Category = Stage,meta = (AllowPrivateAccess = "true"))
+		TObjectPtr<class UStaticMeshComponent> Stage;
+	UPROPERTY(VisibleAnywhere,Category = Stage,meta = (AllowPrivateAccess = "true"))
+		TObjectPtr<class UBoxComponent> StageTrigger;
+
+	//스테이지 트리거의 오버랩 이벤트 확인을 위한 함수
+		/*DECLARE_DYNAMIC_MULTICAST_SPARSE_DELEGATE_SixParams(FComponentBeginOverlapSignature,UPrimitiveComponent,OnComponentBeginOverlap,UPrimitiveComponent*,OverlappedComponent,AActor*,OtherActor,UPrimitiveComponent*,OtherComp,int32,OtherBodyIndex,bool,bFromSweep,const FHitResult &,SweepResult);*/
+	void OnStageTriggerBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent
+		,AActor* OtherActor
+		,UPrimitiveComponent* OtherComp
+		,int32 OtherBodyIndex
+		,bool bFromSweep
+		,const FHitResult& SweepResult
+	);
+
+protected:	//GATE SECTION (4개 문의 대한 섹션)
+	// 4개의 게이트(문)가 필요하기 때문에 Map으로 관리.
+	UPROPERTY(VisibleAnywhere,Category = Gate,meta = (AllowPrivateAccess = "true"))
+		TMap<FName,TObjectPtr<class UStaticMeshComponent>> Gates;
+
+	//각 문의 트리거를 배열로 관리
+	UPROPERTY(VisibleAnywhere,Category = Gate,meta = (AllowPrivateAccess = "true"))
+		TArray<TObjectPtr<class UBoxComponent>> GateTriggers;
+
+	//게이트 트리거의 오버랩 이벤트 확인을 위한 함수
+	void OnGateTriggerBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent
+		,AActor* OtherActor
+		,UPrimitiveComponent* OtherComp
+		,int32 OtherBodyIndex
+		,bool bFromSweep
+		,const FHitResult& SweepResult
+	);
 public:
 };
