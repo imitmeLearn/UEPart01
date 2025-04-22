faad8e140db9670fa70eb782a23c84dc11d541b4
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-22 10:22:10 +0900

    0422 - 09 - 스테이지기믹 - 자동생성 액터 기반 클래스 만들기

---
+++ ArenaBattleDemo/Source/ArenaBattleDemo/Gimmick/ABStageGimmick.cpp
@@ -0,0 +1,8 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Gimmick/ABStageGimmick.h"
+
+// Sets default values
+AABStageGimmick::AABStageGimmick()
+{
+}
---
+++ ArenaBattleDemo/Source/ArenaBattleDemo/Gimmick/ABStageGimmick.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ABStageGimmick.generated.h"
+
+UCLASS()
+class ARENABATTLEDEMO_API AABStageGimmick: public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AABStageGimmick();
+
+protected:
+
+public:
+};
