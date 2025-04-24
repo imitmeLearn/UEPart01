89e566375baefb893d5e6cf330281d90df92aa93
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-24 16:57:35 +0900

    0424 - 10 - 게임데이터관리 - nonplayer 메시  INI  변경하며 , 로드

--- ArenaBattleDemo/Source/ArenaBattleDemo/Character/ABCharacterNonPlayer.cpp
+++ ArenaBattleDemo/Source/ArenaBattleDemo/Character/ABCharacterNonPlayer.cpp
@@ -1,9 +1,32 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Character/ABCharacterNonPlayer.h"
+#include "Engine/AssetManager.h"
 
 AABCharacterNonPlayer::AABCharacterNonPlayer()
-{}
+{
+	//메시 설정 안되어 있으니, 커놓고 시작!
+	GetMesh()->SetHiddenInGame(true);
+}
+
+void AABCharacterNonPlayer::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	////메시 로드 요청
+	//NPC Mehsed 목록 값이 제대로 설정됐는지 확인.
+	ensure(NPCMeshes.Num()>0);
+	//UE_LOG(LogTemp, Log, TEXT("NPCMeshes.Num(): %d"), NPCMeshes.Num());
+
+	//랜덤으로 인덱스 선택
+	int32 RandomIndex = FMath::RandRange(0,NPCMeshes.Num()-1);
+
+	//비동기로 메시 요청.
+	NPCMeshHandle =	UAssetManager::Get().GetStreamableManager().RequestAsyncLoad(
+			NPCMeshes[RandomIndex]
+			,FStreamableDelegate::CreateUObject(this,&AABCharacterNonPlayer::NPCMeshLoadCompleted)
+	);
+}
 
 void AABCharacterNonPlayer::SetDead()
 {
@@ -17,4 +40,27 @@ void AABCharacterNonPlayer::SetDead()
 		,DeadEventDelayTime	//타이머 설정 시간.
 		,false	//반복 여부 설정 (반복 안함)
 	);
+}
+
+void AABCharacterNonPlayer::NPCMeshLoadCompleted()
+{
+	//메시 요청 시 반환된 핸들 값이 유효한지 확인.
+	if(NPCMeshHandle.IsValid())
+	{
+		//로드된 에셋을 스캘레틸 메시 에셋으로 변환
+		USkeletalMesh* NPCMesh = Cast<USkeletalMesh>(NPCMeshHandle->GetLoadedAsset());
+
+		//메시 에셋 설정.
+		if(NPCMesh)
+		{
+			//스켈레탈 메시 에셋 설정.
+			GetMesh()->SetSkeletalMesh(NPCMesh);
+
+			//컴포넌트 보이도록 설정.
+			GetMesh()->SetHiddenInGame(false);
+		}
+	}
+
+	//에셋 로드에 사용했던 핸들 해제.
+	NPCMeshHandle->ReleaseHandle();
 }
--- ArenaBattleDemo/Source/ArenaBattleDemo/Character/ABCharacterNonPlayer.h
+++ ArenaBattleDemo/Source/ArenaBattleDemo/Character/ABCharacterNonPlayer.h
@@ -4,12 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "Character/ABCharacterBase.h"
+#include "Engine/StreamableManager.h"
 #include "ABCharacterNonPlayer.generated.h"
 
 /**
  *
  */
-UCLASS()
+UCLASS(config=ArenaBattle)
 class ARENABATTLEDEMO_API AABCharacterNonPlayer: public AABCharacterBase
 {
 	GENERATED_BODY()
@@ -17,5 +18,18 @@ public:
 	AABCharacterNonPlayer();
 
 protected:
+	//랜덤으로 매시 로드 요청을 위한 함수.
+	virtual void PostInitializeComponents() override;
+
 	virtual void SetDead() override;
+
+	//npc 매시 로드 완료될 떄, 호출될 콜백.
+	void NPCMeshLoadCompleted();
+
+	//config 파일에 저장된 경로 값을 배열로 관리.
+	UPROPERTY(config)
+		TArray<FSoftObjectPath> NPCMeshes;
+
+	//배열 채워진 경로를 활용해 비동기로 에셋을 로드할 때 사용.
+	TSharedPtr<FStreamableHandle> NPCMeshHandle;
 };
