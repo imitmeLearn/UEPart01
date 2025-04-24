f39bf111d80187a31fb2e213fec951aa7b4b8754
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-24 11:26:54 +0900

    0424 - 10 - 게임데이터관리 -실글톤 직접 생성

---
+++ ArenaBattleDemo/Source/ArenaBattleDemo/GameData/ABGameSingleton.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ABGameSingleton.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class ARENABATTLEDEMO_API UABGameSingleton: public UObject
+{
+	GENERATED_BODY()
+public:
+	UABGameSingleton();
+};
