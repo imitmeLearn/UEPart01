59a0782a9fdb6f33d6edc710de152879c48c2330
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-24 12:02:40 +0900

    0424 - 10 - 게임데이터관리 -싱글톤 생성 및 getter 작성 ->데이터테이블에셋로드

--- ArenaBattleDemo/Source/ArenaBattleDemo/GameData/ABGameSingleton.cpp
+++ ArenaBattleDemo/Source/ArenaBattleDemo/GameData/ABGameSingleton.cpp
@@ -2,5 +2,65 @@
 
 #include "GameData/ABGameSingleton.h"
 
+DEFINE_LOG_CATEGORY(LogABGameSingleton);
+
 UABGameSingleton::UABGameSingleton()
-{}
+{
+	//데이터 테이블 에셋 로드
+	static ConstructorHelpers::FObjectFinder<UDataTable> DataTableRef(TEXT("/Game/ArenaBattle/GameData/ABCharacterStatTable.ABCharacterStatTable"));
+	if(DataTableRef.Object)
+	{
+		//UDataTable 은 Key, Value 로 관리됨.
+		//Map 으로 사용하지 않고, 배열로 직접 관리.
+		const UDataTable* DataTable = DataTableRef.Object;
+
+		//데이터 테이블이 제대로 초기화 되어 있는지,확인(검증)
+		check(DataTable->GetRowMap().Num() > 0);
+
+		//값 저장.을 위한 임시 배열.
+		TArray<uint8*> ValueArray;	//데이블의 값 자료형이 uint8* 이기에!!!!! 사용함.
+
+		//데이터 테이블의 GenerateValueArray  함수를 사용해 값 만 배열에 저장.
+		DataTable->GetRowMap().GenerateValueArray(ValueArray);
+
+		// Algo 알고리즘을 활용해 값을 변환해 저장.
+		Algo::Transform(
+			ValueArray
+			,CharacterStatTable
+			,[](uint8* Value){ return *reinterpret_cast<FABCharacterStat*>(Value);}
+		);
+
+		// Algo 알고리즘을 활용해 값을 변환해 저장. 대신 할 수 있는 for 문 사용 가능.
+		//for (uint8* Value : ValueArray)
+		//{
+		//    CharacterStatTable.Add(*reinterpret_cast<FABCharacterStat*>(Value));
+		//}
+	}
+	//최대 레벨 값 설정.
+	CharacterMaxLevel = CharacterStatTable.Num();
+
+	//값에 문제가 없는지 확인.
+	ensure(CharacterMaxLevel>0);
+}
+
+UABGameSingleton & UABGameSingleton::Get()
+{
+	//싱글톤 가져오기(말안되는데, 언리얼에서 해주기에 가능한 부분)
+
+	//프로젝트 세팅에 싱글톤으로 지정했기 때문에 가능.
+	//Engine::Get() 이랑 비슷한거
+	UABGameSingleton* Singleton =  Cast<UABGameSingleton>(GEngine->GameSingleton);
+
+	//객체가 유효하면, 참조-레퍼런스로 변환.
+	if(Singleton)
+	{
+		return *Singleton;
+	}
+
+	////도달하면 안되는 부분!
+	//로그 찍어주기 -오류 출력
+	UE_LOG(LogABGameSingleton,Error,TEXT("InValid Game Singleton."));
+
+	//빈객체 반환
+	return *NewObject<UABGameSingleton>();
+}
--- ArenaBattleDemo/Source/ArenaBattleDemo/GameData/ABGameSingleton.h
+++ ArenaBattleDemo/Source/ArenaBattleDemo/GameData/ABGameSingleton.h
@@ -4,8 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
+#include "ABCharacterStat.h"
+
 #include "ABGameSingleton.generated.h"
 
+//로그 카테고리 선언.
+DECLARE_LOG_CATEGORY_EXTERN(LogABGameSingleton,Error,All);
 /**
  *
  */
@@ -15,4 +19,23 @@ class ARENABATTLEDEMO_API UABGameSingleton: public UObject
 	GENERATED_BODY()
 public:
 	UABGameSingleton();
+
+	//싱글톤 접근 Get
+	static UABGameSingleton& Get();
+public:	//CHARACTER STAT DATA SECTION
+
+	//레벨을 인덱스로 사용해 캐릭터 스탯을 가져올 수 있는 getter
+	FORCEINLINE FABCharacterStat GetCharacterStat(int32 InLevel) const
+	{
+		return CharacterStatTable.IsValidIndex(InLevel -1)
+			? CharacterStatTable[InLevel -1]
+			: FABCharacterStat();
+	}
+
+	UPROPERTY()
+		int32 CharacterMaxLevel;	//최대레벨
+
+private:
+	//스텟을 배열로 관리
+	TArray<FABCharacterStat> CharacterStatTable;
 };
