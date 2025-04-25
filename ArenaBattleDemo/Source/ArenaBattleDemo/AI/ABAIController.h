9e3c13059f9acde7e2c488943a842cec70147047
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-25 10:45:21 +0900

    0425 - 11. BT - aicontroller 생성

---
+++ ArenaBattleDemo/Source/ArenaBattleDemo/AI/ABAIController.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.//
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ABAIController.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class ARENABATTLEDEMO_API AABAIController: public AAIController
+{
+	GENERATED_BODY()
+public:
+};
