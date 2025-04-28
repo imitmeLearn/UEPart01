59b0f4624157b5628cd15f574b34a45f3fd3bb42
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-28 10:20:10 +0900

    0428 - 12. BT : aicontroller - 패트롤 0 인터페이스 생성

---
+++ ArenaBattleDemo/Source/ArenaBattleDemo/Interface/ABCharacterAIInterface.cpp
@@ -0,0 +1,5 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.//
+
+#include "Interface/ABCharacterAIInterface.h"
+
+// Add default functionality here for any IABCharacterAIInterface functions that are not pure virtual.
