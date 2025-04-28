af0b162010dd3bb9d5a608d07107cac4346cb465
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-28 10:32:00 +0900

    0428 - 12. BT : aicontroller - BTService_Detect 생성

---
+++ ArenaBattleDemo/Source/ArenaBattleDemo/AI/MyBTService_Detect.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.//
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "MyBTService_Detect.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class ARENABATTLEDEMO_API UMyBTService_Detect: public UBTService
+{
+	GENERATED_BODY()
+};
