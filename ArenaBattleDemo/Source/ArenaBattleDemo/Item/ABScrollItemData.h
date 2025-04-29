28816e3a7e13aa43a9f80540d1631fb59f73c303
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-29 15:21:19 +0900

    0429- 14. 게임플로우 다듬기 - 아이템 생성

---
+++ ArenaBattleDemo/Source/ArenaBattleDemo/Item/ABScrollItemData.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.//
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item/ABItemData.h"
+#include "ABScrollItemData.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class ARENABATTLEDEMO_API UABScrollItemData: public UABItemData
+{
+	GENERATED_BODY()
+};
