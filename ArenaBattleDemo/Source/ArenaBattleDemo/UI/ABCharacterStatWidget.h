190489e22403877335f99dd8e6c5d54a3de78bab
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-29 11:19:33 +0900

    0429- 13. UI 생성 - 스탯위젯

---
+++ ArenaBattleDemo/Source/ArenaBattleDemo/UI/ABCharacterStatWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.//
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ABCharacterStatWidget.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class ARENABATTLEDEMO_API UABCharacterStatWidget: public UUserWidget
+{
+	GENERATED_BODY()
+};
