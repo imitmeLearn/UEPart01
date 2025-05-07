42c67ca8fe4b8ae1f8dc409f9b4d30af02e1113d
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-05-07 14:41:19 +0900

    0507 - 15. 점수 저장 - 생성

---
+++ ArenaBattleDemo/Source/ArenaBattleDemo/Player/ABSaveGame.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.//
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "ABSaveGame.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class ARENABATTLEDEMO_API UABSaveGame: public USaveGame
+{
+	GENERATED_BODY()
+public:
+};
