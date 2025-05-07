dcef9576862613ab5f88d3ccdcf5d8e1e0005ab6
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-05-07 10:39:05 +0900

    0507 - 15. 게임점수를 위한 정의 및 호출 - 인터페이스 생성

---
+++ ArenaBattleDemo/Source/ArenaBattleDemo/Interface/ABGameInterface.cpp
@@ -0,0 +1,5 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings./
+
+#include "Interface/ABGameInterface.h"
+
+// Add default functionality here for any IABGameInterface functions that are not pure virtual.
